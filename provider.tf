b429dca0585b214c4688e2c0299e9f233ca6b017
ChineduVickreg <nwachukwuvictorc@gmail.com>
2024-01-13 17:54:01 +0100

    updated the provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "cicd-darey"
+    bucket = "terraform-s3bucket-for-practice"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
