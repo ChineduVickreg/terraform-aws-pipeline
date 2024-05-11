115f2a722826fb0861078f3a69aa118f8982d281
ChineduVickreg <nwachukwuvictorc@gmail.com>
2024-05-11 14:29:43 +0100

    updated the terraform files

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.45.0"
+      version = "~> 5.48.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
