87772edf506b5f64605ec4ee620689ebe6e264e7
ChineduVickreg <nwachukwuvictorc@gmail.com>
2024-05-11 14:39:06 +0100

    updated the terraform files

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = ">= 0.12" 
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
