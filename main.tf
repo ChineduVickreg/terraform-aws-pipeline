8b6604f89a2301395a0485b1c16a6ad9129362a7
ChineduVickreg <nwachukwuvictorc@gmail.com>
2024-05-12 20:26:56 +0100

    updated terraform files

--- main.tf
+++ main.tf
@@ -384,17 +384,18 @@ module "ebs_kms_key" {
 
   description = "Customer managed key to encrypt EKS managed node group volumes"
 
+  key_service_roles_for_autoscaling = [
+      # required for the ASG to manage encrypted volumes for nodes
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
+      # required for the cluster / persistentvolume-controller to create encrypted PVCs
+      module.eks.cluster_iam_role_arn
+      # Add other roles as needed
+   ]
+
   # Policy
   key_administrators = [
     data.aws_caller_identity.current.arn
   ]
-   key_service_roles_for_autoscaling = [
-      # required for the ASG to manage encrypted volumes for nodes
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-      # required for the cluster / persistentvolume-controller to create encrypted PVCs
-      module.eks.cluster_iam_role_arn,
-      # Add other roles as needed
-   ]
   # Aliases
   aliases = ["eks/${local.name}/ebs"]
 
